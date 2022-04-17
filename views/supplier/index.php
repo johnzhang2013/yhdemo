0d9a531bae5db0e060899c7f58642b8201e11f01
Johnzhang <zhangzhenxiang@126.com>
2022-04-17 16:22:21 +0800

    Use the same column definitions for both GridView and Exports

--- views/supplier/index.php
+++ views/supplier/index.php
@@ -13,6 +13,92 @@ use kartik\export\ExportMenu;
 
 $this->title = '系统供应商';
 $this->params['breadcrumbs'][] = $this->title;
+$this->gvColumns = [
+    [
+        "class" => CheckboxColumn::className(),
+        "name" => "id",
+    ],            
+            
+    [
+        'label' => '供应商ID',
+        'headerOptions' => ['width' => '10%'],
+        'filter' => true,
+        'attribute' => 'id',
+        'enableSorting' => false,
+        'format' => 'text'
+    ],
+
+    [
+        'label' => '供应商代号',
+        'headerOptions' => ['width' => '10%'],
+        'filter' => true,
+        'attribute' => 'code',
+        'enableSorting' => false,
+        'format' => 'text'
+    ],
+            
+    [
+        'label' => '供应商名称',
+        'headerOptions' => ['width' => '20%'],
+        'attribute' => 'name',
+        'format' => 'text'
+    ],
+            
+    [
+        'label' => '合作状态',
+        'attribute' => 't_status',
+        'filter' => app\models\Supplier::dropDownSelect('t_status'),
+        'value' => function($model){
+            return app\models\Supplier::dropDownSelect('t_status', $model->t_status);
+        }
+    ],
+            
+    [
+        'label' => '所在省份',
+        'headerOptions' => ['width' => '15%'],
+        'attribute' => 'province',
+        'enableSorting' => false
+    ],
+
+    [
+        'label' => '所在城市',
+        'headerOptions' => ['width' => '10%'],
+        'attribute' => 'city',
+        'enableSorting' => false,
+        'value' => function($model){
+            return "{$model->city}市";
+        }
+    ],
+
+    [
+        'label' => '账期[天]',
+        'attribute' => 'account_period',
+        'filter' => app\models\Supplier::dropDownSelect('account_period'),
+        'value' => function($model){
+            return app\models\Supplier::dropDownSelect('account_period', $model->account_period);
+        }
+    ],
+
+    [
+        'label' => '应付账款',
+        'attribute' => 'account_payable_total',
+        'headerOptions' => ['style'=>'color:red'],
+        'contentOptions' => ['style'=>'color:blue'],
+        'value' => function($model){
+            return "￥{$model->account_payable_total}";
+        }
+    ],
+
+    [
+        'class' => ActionColumn::className(),
+        'headerOptions' => ['width' => '10%'],
+        'header' => '操作',
+        'template'=> '{view} {update}',
+        'urlCreator' => function ($action, app\models\Supplier $model, $key, $index, $column) {
+            return Url::toRoute([$action, 'id' => $model->id]);
+         }
+    ],
+];
 ?>
 <div class="supplier-index">
     <h1><?= Html::encode($this->title) ?></h1>
@@ -22,97 +108,12 @@ $this->params['breadcrumbs'][] = $this->title;
         'filterModel' => $searchModel,
         'options' => ['class' => 'grid-view','style'=>'overflow:auto', 'id' => 'supplier_container'],
         'layout' => "{summary}\n{items}\n{pager}",
-        'columns' => [
-            [
-                "class" => CheckboxColumn::className(),
-                "name" => "id",
-            ],            
-            
-            [
-                'label' => '供应商ID',
-                'headerOptions' => ['width' => '10%'],
-                'filter' => true,
-                'attribute' => 'id',
-                'enableSorting' => false,
-                'format' => 'text'
-            ],
-
-            [
-                'label' => '供应商代号',
-                'headerOptions' => ['width' => '10%'],
-                'filter' => true,
-                'attribute' => 'code',
-                'enableSorting' => false,
-                'format' => 'text'
-            ],
-            
-            [
-                'label' => '供应商名称',
-                'headerOptions' => ['width' => '20%'],
-                'attribute' => 'name',
-                'format' => 'text'
-            ],
-            
-            [
-                'label' => '合作状态',
-                'attribute' => 't_status',
-                'filter' => app\models\Supplier::dropDownSelect('t_status'),
-                'value' => function($model){
-                    return app\models\Supplier::dropDownSelect('t_status', $model->t_status);
-                }
-            ],
-            
-            [
-                'label' => '所在省份',
-                'headerOptions' => ['width' => '15%'],
-                'attribute' => 'province',
-                'enableSorting' => false
-            ],
-
-            [
-                'label' => '所在城市',
-                'headerOptions' => ['width' => '10%'],
-                'attribute' => 'city',
-                'enableSorting' => false,
-                'value' => function($model){
-                    return "{$model->city}市";
-                }
-            ],
-
-            [
-                'label' => '账期[天]',
-                'attribute' => 'account_period',
-                'filter' => app\models\Supplier::dropDownSelect('account_period'),
-                'value' => function($model){
-                    return app\models\Supplier::dropDownSelect('account_period', $model->account_period);
-                }
-            ],
-
-            [
-                'label' => '应付账款',
-                'attribute' => 'account_payable_total',
-                'headerOptions' => ['style'=>'color:red'],
-                'contentOptions' => ['style'=>'color:blue'],
-                'value' => function($model){
-                    return "￥{$model->account_payable_total}";
-                }
-            ],
-
-            [
-                'class' => ActionColumn::className(),
-                'headerOptions' => ['width' => '10%'],
-                'header' => '操作',
-                'template'=> '{view} {update}',
-                'urlCreator' => function ($action, app\models\Supplier $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
-            ],
-        ]
+        'columns' => $this->gvColumns
     ]);?>
 
     <?= ExportMenu::widget([
         'dataProvider' => $dataProvider,
-        'columns' => $gridColumns,
+        'columns' => $this->gvColumns,
         'encoding' => 'gb2312',
         'dropdownOptions' => [
             'label' => '导出',
