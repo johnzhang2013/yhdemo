1467a64841862475871046fe18da571abba0b5a8
Johnzhang <zhangzhenxiang@126.com>
2022-04-17 16:30:50 +0800

    Keep going...

--- controllers/SupplierController.php
+++ controllers/SupplierController.php
@@ -12,6 +12,10 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 
+use yii\grid\CheckboxColumn;
+use yii\grid\ActionColumn;
+use yii\helpers\Url;
+
 /**
  * SupplierController implements the CRUD actions for Supplier model.
  */
@@ -44,24 +48,12 @@ class SupplierController extends Controller
     {
         $searchModel = new SupplierSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->get());
-        $dataProvider->setSort(false);
-
-        /*$dataProvider = new ActiveDataProvider([
-            'query' => Supplier::find(),
-            
-            'pagination' => [
-                'pageSize' => 50
-            ],
-            'sort' => [
-                'defaultOrder' => [
-                    'id' => SORT_DESC,
-                ]
-            ],            
-        ]);*/
+        $dataProvider->setSort(false);        
 
         return $this->render('index', [
             'dataProvider' => $dataProvider,
             'searchModel' => $searchModel,
+            'gvColumns' => $this->makeGridViewColumns()
         ]);
     }
 
@@ -148,5 +140,94 @@ class SupplierController extends Controller
         }
 
         throw new NotFoundHttpException('The requested page does not exist.');
+    }
+
+    protected function makeGridViewColumns(){
+        return [
+            [
+                "class" => CheckboxColumn::className(),
+                "name" => "id",
+            ],            
+            
+            [
+                'label' => '供应商ID',
+                'headerOptions' => ['width' => '10%'],
+                'filter' => true,
+                'attribute' => 'id',
+                'enableSorting' => false,
+                'format' => 'text'
+            ],
+
+            [
+                'label' => '供应商代号',
+                'headerOptions' => ['width' => '10%'],
+                'filter' => true,
+                'attribute' => 'code',
+                'enableSorting' => false,
+                'format' => 'text'
+            ],
+                    
+            [
+                'label' => '供应商名称',
+                'headerOptions' => ['width' => '20%'],
+                'attribute' => 'name',
+                'format' => 'text'
+            ],
+                    
+            [
+                'label' => '合作状态',
+                'attribute' => 't_status',
+                'filter' => Supplier::dropDownSelect('t_status'),
+                'value' => function($model){
+                    return Supplier::dropDownSelect('t_status', $model->t_status);
+                }
+            ],
+                    
+            [
+                'label' => '所在省份',
+                'headerOptions' => ['width' => '15%'],
+                'attribute' => 'province',
+                'enableSorting' => false
+            ],
+
+            [
+                'label' => '所在城市',
+                'headerOptions' => ['width' => '10%'],
+                'attribute' => 'city',
+                'enableSorting' => false,
+                'value' => function($model){
+                    return "{$model->city}市";
+                }
+            ],
+
+            [
+                'label' => '账期[天]',
+                'attribute' => 'account_period',
+                'filter' => Supplier::dropDownSelect('account_period'),
+                'value' => function($model){
+                    return Supplier::dropDownSelect('account_period', $model->account_period);
+                }
+            ],
+
+            [
+                'label' => '应付账款',
+                'attribute' => 'account_payable_total',
+                'headerOptions' => ['style'=>'color:red'],
+                'contentOptions' => ['style'=>'color:blue'],
+                'value' => function($model){
+                    return "￥{$model->account_payable_total}";
+                }
+            ],
+
+            [
+                'class' => ActionColumn::className(),
+                'headerOptions' => ['width' => '10%'],
+                'header' => '操作',
+                'template'=> '{view} {update}',
+                'urlCreator' => function ($action, Supplier $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ];
     }
 }
--- views/supplier/index.php
+++ views/supplier/index.php
@@ -1,10 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use yii\helpers\Url;
-use yii\grid\ActionColumn;
-use yii\grid\CheckboxColumn;
-//use yii\grid\GridView;
+
 use kartik\grid\GridView;
 use kartik\export\ExportMenu;
 
@@ -13,92 +10,7 @@ use kartik\export\ExportMenu;
 
 $this->title = '系统供应商';
 $this->params['breadcrumbs'][] = $this->title;
-$this->gvColumns = [
-    [
-        "class" => CheckboxColumn::className(),
-        "name" => "id",
-    ],            
-            
-    [
-        'label' => '供应商ID',
-        'headerOptions' => ['width' => '10%'],
-        'filter' => true,
-        'attribute' => 'id',
-        'enableSorting' => false,
-        'format' => 'text'
-    ],
-
-    [
-        'label' => '供应商代号',
-        'headerOptions' => ['width' => '10%'],
-        'filter' => true,
-        'attribute' => 'code',
-        'enableSorting' => false,
-        'format' => 'text'
-    ],
-            
-    [
-        'label' => '供应商名称',
-        'headerOptions' => ['width' => '20%'],
-        'attribute' => 'name',
-        'format' => 'text'
-    ],
-            
-    [
-        'label' => '合作状态',
-        'attribute' => 't_status',
-        'filter' => app\models\Supplier::dropDownSelect('t_status'),
-        'value' => function($model){
-            return app\models\Supplier::dropDownSelect('t_status', $model->t_status);
-        }
-    ],
-            
-    [
-        'label' => '所在省份',
-        'headerOptions' => ['width' => '15%'],
-        'attribute' => 'province',
-        'enableSorting' => false
-    ],
-
-    [
-        'label' => '所在城市',
-        'headerOptions' => ['width' => '10%'],
-        'attribute' => 'city',
-        'enableSorting' => false,
-        'value' => function($model){
-            return "{$model->city}市";
-        }
-    ],
-
-    [
-        'label' => '账期[天]',
-        'attribute' => 'account_period',
-        'filter' => app\models\Supplier::dropDownSelect('account_period'),
-        'value' => function($model){
-            return app\models\Supplier::dropDownSelect('account_period', $model->account_period);
-        }
-    ],
-
-    [
-        'label' => '应付账款',
-        'attribute' => 'account_payable_total',
-        'headerOptions' => ['style'=>'color:red'],
-        'contentOptions' => ['style'=>'color:blue'],
-        'value' => function($model){
-            return "￥{$model->account_payable_total}";
-        }
-    ],
-
-    [
-        'class' => ActionColumn::className(),
-        'headerOptions' => ['width' => '10%'],
-        'header' => '操作',
-        'template'=> '{view} {update}',
-        'urlCreator' => function ($action, app\models\Supplier $model, $key, $index, $column) {
-            return Url::toRoute([$action, 'id' => $model->id]);
-         }
-    ],
-];
+$this->gvColumns = ;
 ?>
 <div class="supplier-index">
     <h1><?= Html::encode($this->title) ?></h1>
@@ -108,12 +20,12 @@ $this->gvColumns = [
         'filterModel' => $searchModel,
         'options' => ['class' => 'grid-view','style'=>'overflow:auto', 'id' => 'supplier_container'],
         'layout' => "{summary}\n{items}\n{pager}",
-        'columns' => $this->gvColumns
+        'columns' => $gvColumns
     ]);?>
 
     <?= ExportMenu::widget([
         'dataProvider' => $dataProvider,
-        'columns' => $this->gvColumns,
+        'columns' => $gvColumns,
         'encoding' => 'gb2312',
         'dropdownOptions' => [
             'label' => '导出',
